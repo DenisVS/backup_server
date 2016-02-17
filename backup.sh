38844ee266d8cf8e5d0ab93de80ce481ac30f203
DenisVS <denvsim@gmail.com>
2016-02-17 09:35:35 +0000

    Выбор архиватора для архивов и дампов

--- backup.sh
+++ backup.sh
@@ -39,21 +39,21 @@ touch ${DIR}/${TIMENAME}/base/mysql/users.sql
 chmod -R 777 ${DIR}/${TIMENAME}
 
 # Сохраняем системную конфигурацию
-tar cvfz ${DIR}/${TIMENAME}/backup_system.tar.gz /etc /usr/local/etc /boot/loader.conf /var/cron/tabs /var/db/mysql/my.cnf /root/.ssh 
-chmod 666 ${DIR}/${TIMENAME}/backup_system.tar.gz
+tar cvf${TAR_OPT} ${DIR}/${TIMENAME}/backup_system.tar.${ARCH_EXT} /etc /usr/local/etc /boot/loader.conf /var/cron/tabs /var/db/mysql/my.cnf /root/.ssh 
+chmod 666 ${DIR}/${TIMENAME}/backup_system.tar.${ARCH_EXT}
 
 
 TIMEDUMP=`date '+%T %x'`
-echo "backup has been done at $TIMEDUMP on backup_system.tgz" >> ${LOG}
+echo "backup has been done at $TIMEDUMP on backup_system.tar.${ARCH_EXT}" >> ${LOG}
 
 #cp /usr/local/etc/apache22/httpd.conf.maintenance  /usr/local/etc/apache22/httpd.conf
 #/usr/local/sbin/apachectl restart
 
 ##### Сохраняем WWW (Старая версия, все сайты в кучу)
-#tar cvfz ${DIR}/${TIMENAME}/backup_www.tar.gz ${WWW}
-#chmod 666 ${DIR}/${TIMENAME}/backup_www.tar.gz
+#tar cvf${TAR_OPT} ${DIR}/${TIMENAME}/backup_www.tar.${ARCH_EXT} ${WWW}
+#chmod 666 ${DIR}/${TIMENAME}/backup_www.tar.${ARCH_EXT}
 #TIMEDUMP=`date '+%T %x'`
-#echo "backup has been done at $TIMEDUMP on backup_www.tgz" >> ${LOG}
+#echo "backup has been done at $TIMEDUMP on backup_www.tar.${ARCH_EXT}" >> ${LOG}
 
 ##### Сохраняем WWW (Новая версия, каждый сайт в отдельной директории)
 /bin/mkdir -p ${DIR}/${TIMENAME}/www
@@ -63,30 +63,30 @@ IFS='
 '
 for CURRENT_DIR in ${LIST_OF_DIRS}; do
     echo Pack directory ${CURRENT_DIR}
-    tar cvfz ${DIR}/${TIMENAME}/www/${CURRENT_DIR}.tar.gz ${WWW}/${CURRENT_DIR}
-    chmod 666 ${DIR}/${TIMENAME}/www/${CURRENT_DIR}.tar.gz
+    tar cvf${TAR_OPT} ${DIR}/${TIMENAME}/www/${CURRENT_DIR}.tar.${ARCH_EXT} ${WWW}/${CURRENT_DIR}
+    chmod 666 ${DIR}/${TIMENAME}/www/${CURRENT_DIR}.tar.${ARCH_EXT}
 done
 IFS=$SAVEIFS
 TIMEDUMP=`date '+%T %x'`
-echo "backup has been done at $TIMEDUMP on backup_www.tgz" >> ${LOG}
+echo "backup has been done at $TIMEDUMP on backup_www.tar.${ARCH_EXT}" >> ${LOG}
 
 # Закатываем базу Radicale
-tar cvfz ${DIR}/${TIMENAME}/base/backup_radicale.tar.gz /var/db/radicale
-chmod 666 ${DIR}/${TIMENAME}/base/backup_radicale.tar.gz
+tar cvf${TAR_OPT} ${DIR}/${TIMENAME}/base/backup_radicale.tar.${ARCH_EXT} /var/db/radicale
+chmod 666 ${DIR}/${TIMENAME}/base/backup_radicale.tar.${ARCH_EXT}
 TIMEDUMP=`date '+%T %x'`
-echo "backup has been done at $TIMEDUMP on backup_radicale.tar.gz" >> ${LOG}
+echo "backup has been done at $TIMEDUMP on backup_radicale.tar.${ARCH_EXT}" >> ${LOG}
 
 # Закатываем SVN
-tar cvfz ${DIR}/${TIMENAME}/base/backup_svn.tar.gz /data/svn
-chmod 666 ${DIR}/${TIMENAME}/base/backup_svn.tar.gz
+tar cvf${TAR_OPT} ${DIR}/${TIMENAME}/base/backup_svn.tar.${ARCH_EXT} /data/svn
+chmod 666 ${DIR}/${TIMENAME}/base/backup_svn.tar.${ARCH_EXT}
 TIMEDUMP=`date '+%T %x'`
-echo "backup has been done at $TIMEDUMP on backup_svn.tar.gz" >> ${LOG}
+echo "backup has been done at $TIMEDUMP on backup_svn.tar.${ARCH_EXT}" >> ${LOG}
 
 # Закатываем TRAC
-tar cvfz ${DIR}/${TIMENAME}/base/backup_trac.tar.gz /data/trac
-chmod 666 ${DIR}/${TIMENAME}/base/backup_trac.tar.gz
+tar cvf${TAR_OPT} ${DIR}/${TIMENAME}/base/backup_trac.tar.${ARCH_EXT} /data/trac
+chmod 666 ${DIR}/${TIMENAME}/base/backup_trac.tar.${ARCH_EXT}
 TIMEDUMP=`date '+%T %x'`
-echo "backup has been done at $TIMEDUMP on backup_trac.tar.gz" >> ${LOG}
+echo "backup has been done at $TIMEDUMP on backup_trac.tar.${ARCH_EXT}" >> ${LOG}
 
 
 #cp /usr/local/etc/apache22/httpd.conf.current  /usr/local/etc/apache22/httpd.conf
@@ -103,8 +103,8 @@ for DBNAME in ${DBASES}; do
 	done
     fi
     if [ "${ARCH_BASE}" = "1" ]; then
-	/usr/local/bin/mysqldump -u${DBUSER} -h ${DBHOST} -p${DBPASS} ${DBNAME} | /usr/bin/gzip -c > "${DIR}/${TIMENAME}/base/mysql/${DBNAME}.sql.gz" 
-	chmod 666 ${DIR}/${TIMENAME}/base/mysql/${DBNAME}.sql.gz
+	/usr/local/bin/mysqldump -u${DBUSER} -h ${DBHOST} -p${DBPASS} ${DBNAME} | ${ARCH_APP} -c > "${DIR}/${TIMENAME}/base/mysql/${DBNAME}.sql.${ARCH_EXT}" 
+	chmod 666 ${DIR}/${TIMENAME}/base/mysql/${DBNAME}.sql.${ARCH_EXT}
     else
 	/usr/local/bin/mysqldump -u${DBUSER} -h ${DBHOST} -p${DBPASS} ${DBNAME} > "${DIR}/${TIMENAME}/base/mysql/${DBNAME}.sql" 
 	chmod 666 ${DIR}/${TIMENAME}/base/mysql/${DBNAME}.sql
@@ -116,7 +116,6 @@ done
 ##### Сохраняем GRANTS
 USERS_HOSTS=`/usr/local/bin/mysql -u ${DBUSER} -B -N -p"${DBPASS}" -e "SELECT user, host FROM user" mysql`
 
-# \todo Тестируем, перенёс ниже
 SAVEIFS=$IFS
 IFS='
 '
